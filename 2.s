93b13f6184a21263d298df15f405770cdbe7deff
matheusflavio <flavio.matheusgsilva@gmail.com>
2022-10-23 20:25:29 -0300

    Changing x14 value and calling verificacpf instead of verificadastro

--- 2.s
+++ 2.s
@@ -4,8 +4,8 @@ vetor: .word 1 2 3 4 5 6 7 8 9 10 11
 main:
 la x12, vetor
 addi x13, x0, 11
-addi x14, x0, 1
-jal x1, verificadastro
+addi x14, x0, 0
+jal x1, verificacpf
 beq x0,x0,FIM
 
 ##### START MODIFIQUE AQUI START #####
